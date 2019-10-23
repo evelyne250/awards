41a574807cf4dec01516d66819d6502e20c775a3
pascaline <passyish5@gmail.com>
2019-10-23 18:02:15 +0200

    connected our view function to a URL

--- award/views.py
+++ award/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
+from django.http  import HttpResponse
 
 # Create your views here.
 def welcome(request):
-    date = dt.date.today()
-    return render(welcome.html,{"date":date})
+    return HttpResponse('Welcome to the Moringa Tribune')
