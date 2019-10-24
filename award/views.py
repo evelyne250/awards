8dde41625b6887c740d2f26f69a24186fb325b8e
pascaline <passyish5@gmail.com>
2019-10-24 09:44:32 +0200

    added forms to register and login

--- award/views.py
+++ award/views.py
@@ -1,6 +1,8 @@
 from django.http  import HttpResponse
 import datetime as dt
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 def welcome(request):
     date = dt.date.today()
