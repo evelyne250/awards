a264c649b5aa41e1090eaf0faf343cc4b703e29e
pascaline <passyish5@gmail.com>
2019-10-24 14:57:54 +0200

    added the project model

--- award/models.py
+++ award/models.py
@@ -4,9 +4,12 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
-    # user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     profile_picture = models.ImageField(upload_to='images/')
     bio = models.TextField(max_length=500)
+    name = models.CharField(max_length=120)
+    def __str__(self):
+        return self.name
 
 class Project(models.Model):
     title = models.CharField(max_length=155)
